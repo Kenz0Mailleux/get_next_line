4a5ac1939579c2bf318bc74e7eb47a4d5e0f8e03
Kenzo Mailleux <kmailleu@shi-r1-p2.s19.be>
2024-03-28 20:28:56 +0100

    fini

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmailleu <kmailleu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 16:36:16 by kenzo             #+#    #+#             */
-/*   Updated: 2024/03/28 20:09:33 by kmailleu         ###   ########.fr       */
+/*   Updated: 2024/03/28 20:18:41 by kmailleu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/uio.h>
